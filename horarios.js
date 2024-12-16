56372ffd7333f6393d6bb41de83ddcae4933a861
Carlos Padilla <carlosfpm-@hotmail.com>
2024-12-16 18:11:05 -0500

    pagina finalizada

--- horarios.js
+++ horarios.js
@@ -17,18 +17,4 @@ router.get('/', async (req, res) => {
     }
 });
 
-// Ruta para crear una nueva reserva
-router.post('/reservar', async (req, res) => {
-    const { usuario_id, cancha_id, horario } = req.body;
-    const fecha_actual = new Date().toISOString().slice(0, 19).replace('T', ' '); // Formato YYYY-MM-DD HH:MM:SS
-    try {
-        // Inserta la nueva reserva en la base de datos
-        await turso.execute('INSERT INTO reservas (usuario_id, cancha_id, fecha, horario) VALUES (?, ?, ?, ?)', [usuario_id, cancha_id, fecha_actual, horario]);
-        res.status(201).json({ success: true, message: 'Reserva creada exitosamente' });
-    } catch (err) {
-        console.error('Error en el servidor:', err);
-        res.status(500).json({ success: false, message: 'Error del servidor' });
-    }
-});
-
 export default router;
--- registerReservations.js
+++ registerReservations.js
@@ -4,20 +4,39 @@ import { turso } from './lib/connect.js';
 
 const router = express.Router();
 
+const getNextReservaId = async () => {
+    try {
+        const result = await turso.execute('SELECT MAX(reserva_id) as maxId FROM reservas');
+        const maxId = result.rows[0].maxId;
+        return maxId ? maxId + 1 : 1;
+    } catch (err) {
+        console.error('Error al obtener el próximo reserva_id:', err);
+        throw new Error('Error al obtener el próximo reserva_id');
+    }
+};
+
 router.post('/', async (req, res) => {
-    const { usuario_id, cancha_id, fecha, hora_inicio, hora_fin, estado } = req.body;
+    const { reserva_id, usuario_id, cancha_id, fecha, hora_inicio, hora_fin, estado } = req.body;
 
     try {
+        const reserva_id = await getNextReservaId();
+
         // Insertar reserva en la base de datos
         await turso.execute(
-            'INSERT INTO reservas (usuario_id, cancha_id, fecha, hora_inicio, hora_fin, estado) VALUES (?, ?, ?, ?, ?, ?)',
-            [usuario_id, cancha_id, fecha, hora_inicio, hora_fin, estado]
+            'INSERT INTO reservas (reserva_id, usuario_id, cancha_id, fecha, hora_inicio, hora_fin, estado) VALUES (?, ?, ?, ?, ?, ?, ?)',
+            [reserva_id, usuario_id, cancha_id, fecha, hora_inicio, hora_fin, estado]
+        );
+
+        //Cambiar la disponibilidad de la cancha
+        await turso.execute(
+            'UPDATE canchas_horario SET disponibilidad = 0 WHERE cancha_id = ? AND horario_inicio = ?',
+            [cancha_id, hora_inicio]
         );
 
         res.status(201).json({ success: true, message: 'Reserva registrada' });
     } catch (err) {
         console.error('Error en el servidor:', err);
-        res.status(500).json({ success: false, message: 'Error del servidor' });
+        res.status(500).json({ success: false, message: 'Error del servidor de reservas' });
     }
 });
 
